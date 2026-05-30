f41577b070ad211ce6c861cc3f182f89aefcb93b
Akash Maity <am6030920@gmail.com>
2026-05-30 21:25:48 +0530

    Dark and light button update

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,20 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { ThemeProvider } from "./context/ThemeContext";
 import Home from "./assets/component/Home";
 import Login from "./assets/component/auth/Login";
 import Final from "./assets/component/Final";
+
 const App = () => {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/Final" element={<Final />} />
-      </Routes>
-    </BrowserRouter>
+    <ThemeProvider>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/Final" element={<Final />} />
+        </Routes>
+      </BrowserRouter>
+    </ThemeProvider>
   );
 };
 
